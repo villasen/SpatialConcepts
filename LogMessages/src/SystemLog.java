9e84c115ed42607787af4208b9bf1ca0d85a17e4
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-10-10 21:24:38 -0700

    Add SystemLog class

---
+++ LogMessages/src/SystemLog.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SystemLog {
+
+        private List<LogMessage> messageList;
+
+        public List<LogMessage> removeMessages(String keyword){
+
+          return  messageList;
+
+        }
+
+
+}
