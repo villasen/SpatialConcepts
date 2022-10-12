3a20f70dc9c1ba0952dcd33443acbbff76430796
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-10-11 21:04:04 -0700

    Added removals object

--- LogMessages/src/SystemLog.java
+++ LogMessages/src/SystemLog.java
@@ -4,9 +4,20 @@ public class SystemLog {
 
         private List<LogMessage> messageList;
 
+
         public List<LogMessage> removeMessages(String keyword){
 
-          return  messageList;
+            // create object to be returned by method
+            List<LogMessage> removals = new ArrayList<LogMessage>();
+
+            for( int i=0; i < messageList.size(); i++){
+
+                if(messageList.get(i).containsWord(keyword)){
+                    removals.add(messageList.remove(i));
+                    i--;
+                }
+            }
+            return  removals;
 
         }
 
