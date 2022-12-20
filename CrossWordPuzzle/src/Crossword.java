e0b4b674ef0d91f7cac1c1423435bf4e8ca10a10
Martin Villaseñor <43162903+villasen@users.noreply.github.com>
2022-12-20 09:24:42 -0800

    FrogHop init

---
+++ CrossWordPuzzle/src/Crossword.java
@@ -0,0 +1,15 @@
+public class Crossword {
+
+    private Square[][] puzzle;
+
+    public Crossword(){
+
+    }
+
+    private boolean toBeLabeled(int r, int c, boolean[][] blackSquares){
+        return true;
+    }
+
+
+
+}
---
+++ CrossWordPuzzle/src/Square.java
@@ -0,0 +1,16 @@
+public class Square {
+
+    private boolean isBlack;
+    private int     num;
+
+    public Square(boolean isBlack, int num){
+
+        this.num = num;
+        if (isBlack){
+            this.isBlack = true;
+        }
+    }
+
+
+
+}
